62d5c28310aa39587cae827265aafd574120fb3b
HabibaAmer <habibaamer47@gmail.com>
2021-07-09 21:13:06 +0200

    Linked list updates

--- student/student.c
+++ student/student.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
 
 struct Student
 {
@@ -12,31 +14,32 @@ struct Student
 
 struct Node
 {
-    struct Student* data;
+    struct Student data;
     struct Node* next;
 };
 
-struct Student* readStudent(int i)
+struct Student readStudent(int i)
 {
-    struct Student* student = (struct Student*)malloc(sizeof(struct Student));
+    struct Student student;
+
     printf("\nPlease Insert Student %d\n", i + 1);
 
     printf("Enter name: ");
-    scanf("%s", student->name);
+    scanf("%s", student.name);
 
     printf("Enter ID: ");
-    scanf("%d", &student->id);
+    scanf("%d", &student.id);
 
     printf("Enter Birthday: ");
-    scanf("%d/%d/%d", &student->birthdate[0], &student->birthdate[1], &student->birthdate[2]);
+    scanf("%d/%d/%d", &student.birthdate[0], &student.birthdate[1], &student.birthdate[2]);
 
     printf("Enter Score: ");
-    scanf("%d", &student->score);
+    scanf("%d", &student.score);
 
     return student;
 };
 
-void insertBeginningLinkedList(struct Node** head, struct Student* student)
+void insertBeginningLinkedList(struct Node** head, struct Student student)
 {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = student;
@@ -44,7 +47,7 @@ void insertBeginningLinkedList(struct Node** head, struct Student* student)
     *head = newNode;
 }
 
-void insertEndLinkedList(struct Node** tail, struct Student* student)
+void insertEndLinkedList(struct Node** tail, struct Student student)
 {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = student;
@@ -53,7 +56,7 @@ void insertEndLinkedList(struct Node** tail, struct Student* student)
     *tail = newNode;
 }
 
-void insertPosLinkedList(struct Node* head, struct Student* student, int pos)
+void insertPosLinkedList(struct Node* head, struct Student student, int pos)
 {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = student;
@@ -81,37 +84,37 @@ void printListLinkedList(struct Node* head)
     printf("\nList of Students: \n");
     while(current)
     {
-        printf("%d", current->data->id);
-        printf(" %s", current->data->name);
-        printf(" %d/%d/%d", current->data->birthdate[0], current->data->birthdate[1], current->data->birthdate[2]);
-        printf(" %d\n", current->data->score);
+        printf("%d", current->data.id);
+        printf(" %s", current->data.name);
+        printf(" %d/%d/%d", current->data.birthdate[0], current->data.birthdate[1], current->data.birthdate[2]);
+        printf(" %d\n", current->data.score);
         current = current->next;
     }
 }
 
-void insertBeginningDynamicArray(struct Student* ptr, struct Student* newStud, int N)
+void insertBeginningDynamicArray(struct Student* ptr, struct Student newStud, int N)
 {
     int k;
     for(k=N-1; k>=0; k--)
     {
         *(ptr+k+1)= *(ptr+k);
     }
-    *(ptr + 0) = *newStud;
+    *(ptr + 0) = newStud;
 }
 
-void insertEndDynamicArray(struct Student* ptr, struct Student* newStud, int lastIndex)
+void insertEndDynamicArray(struct Student* ptr, struct Student newStud, int lastIndex)
 {
-    *(ptr+lastIndex) = *newStud;
+    *(ptr+lastIndex) = newStud;
 }
 
-void insertPosDynamicArray(struct Student* ptr, struct Student* newStud, int N,int i)
+void insertPosDynamicArray(struct Student* ptr, struct Student newStud, int N,int i)
 {
     int k;
     for(k=N-1; k>=i; k--)
     {
         *(ptr+k+1)= *(ptr+k);
     }
-    *(ptr + i) = *newStud;
+    *(ptr + i) = newStud;
 }
 
 void printListDynamicArray(struct Student* ptr,int N)
@@ -131,6 +134,73 @@ void printListDynamicArray(struct Student* ptr,int N)
     printf("\n");
 }
 
+void enterNStudentsLinked(int* i, int N, struct Node** head, struct Node** tail)
+{
+    while(*i < N)
+    {
+        struct timeval start, end;
+        struct Student student;
+        char choice;
+        int pos;
+        printf("\nChoose Where you want to insert student %d: \na. At the beginning\nb. At position i\nc. At the end\n", (*i) + 1 );
+        printf("Enter Choice: ");
+        scanf(" %c", &choice);
+        switch (choice)
+        {
+        case 'a': {
+            student = readStudent(*i);
+
+            gettimeofday(&start, NULL);
+
+            insertBeginningLinkedList(*(&head), student);
+
+            gettimeofday(&end, NULL);
+            long seconds = (end.tv_sec - start.tv_sec);
+            long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+
+            printf("\n\nThe elapsed time is %d seconds and %d micros\n", seconds, micros);
+
+            break;
+        }
+        case 'b': {
+            printf("Enter Position: ");
+            scanf("%d", &pos);
+            student = readStudent(*i);
+            gettimeofday(&start, NULL);
+
+            insertPosLinkedList(*(head), student, pos);
+
+            gettimeofday(&end, NULL);
+            long seconds = (end.tv_sec - start.tv_sec);
+            long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+
+            printf("\n\nThe elapsed time is %d seconds and %d micros\n", seconds, micros);
+
+            break;
+        }
+        case 'c': {
+            student = readStudent(*i);
+            gettimeofday(&start, NULL);
+
+
+            insertEndLinkedList(*(&tail), student);
+
+            gettimeofday(&end, NULL);
+            long seconds = (end.tv_sec - start.tv_sec);
+            long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+
+            printf("\n\nThe elapsed time is %d seconds and %d micros\n", seconds, micros);
+            break;
+        }
+        default:
+            printf("Invalid choice\n");
+            (*i)--;
+        }
+        (*i)++;
+        printListLinkedList(*head);
+    }
+}
+
 void startLinkedList()
 {
 
@@ -140,41 +210,60 @@ void startLinkedList()
 
     struct Node* head = (struct Node*)malloc(sizeof(struct Node));
     struct Node* tail = head;
-    struct Student* student = readStudent(0);
+    struct Student student = readStudent(0);
     head->data = student;
     head->next = NULL;
 
     int i = 1;
-    while(i < N)
+    enterNStudentsLinked(&i, N, &head, &tail);
+
+    printf("\nDo you want to add more students? Enter Y to add more or N to stop\n");
+    char choice;
+    printf("Enter Choice: ");
+    scanf(" %c", &choice);
+    while(choice == 'Y')
     {
-        struct Student* student = NULL;
-        char choice;
-        int pos;
-        printf("\nChoose Where you want to insert student %d: \na. At the beginning\nb. At position i\nc. At the end\n", i + 1 );
+        int M;
+        printf("Enter Number of students: ");
+        scanf("%d", &M);
+        N = N + M;
+
+        enterNStudentsLinked(&i, N, &head, &tail);
+
+        printf("\nDo you want to add more students? Enter Y to add more or N to stop\n");
         printf("Enter Choice: ");
         scanf(" %c", &choice);
-        switch (choice)
-        {
-        case 'a':
-            student = readStudent(i);
-            insertBeginningLinkedList(&head, student);
-            break;
-        case 'b':
-            printf("Enter Position: ");
-            scanf("%d", &pos);
-            student = readStudent(i);
-            insertPosLinkedList(head, student, pos);
-            break;
-        case 'c':
-            student = readStudent(i);
-            insertEndLinkedList(&tail, student);
-            break;
-        default:
-            printf("Invalid choice\n");
-            i--;
-        }
-        i++;
-        printListLinkedList(head);
+    }
+
+
+}
+
+void enterStudentDynamicArray(int i, int N, struct Student** ptr)
+{
+    struct Student newStud;
+    char choice;
+    int pos;
+    printf("Choose Where you want to insert: \na. At the beginning\nb. At position i\nc. At the end\n");
+    printf("Enter Choice: ");
+    scanf(" %c", &choice);
+    switch (choice)
+    {
+    case 'a':
+        newStud = readStudent(i);
+        insertBeginningDynamicArray(*ptr, newStud, N);
+        break;
+    case 'b':
+        printf("Enter Position: ");
+        scanf("%d", &pos);
+        newStud = readStudent(i);
+        insertPosDynamicArray(*ptr, newStud, N, pos);
+        break;
+    case 'c':
+        newStud = readStudent(i);
+        insertEndDynamicArray(*ptr, newStud, i);
+        break;
+    default:
+        printf("Invalid choice\n");
     }
 }
 
@@ -186,41 +275,48 @@ void startDynamicArray()
     struct Student* ptr=(struct Student*)calloc(N, sizeof(struct Student));
     for (i=0; i<N; i++)
     {
-        struct Student* newStud;
+        struct Student newStud;
         if(i == 0)
         {
             newStud = readStudent(i);
-            *(ptr+i) = *newStud;
+            *(ptr+i) = newStud;
         }
         else
         {
-            char choice;
-            int pos;
-            printf("Choose Where you want to insert: \na. At the beginning\nb. At position i\nc. At the end\n");
-            printf("Enter Choice: ");
-            scanf(" %c", &choice);
-            switch (choice)
-            {
-            case 'a':
-                newStud = readStudent(i);
-                insertBeginningDynamicArray(ptr, newStud, N);
-                break;
-            case 'b':
-                printf("Enter Position: ");
-                scanf("%d", &pos);
-                newStud = readStudent(i);
-                insertPosDynamicArray(ptr, newStud, N, pos);
-                break;
-            case 'c':
-                newStud = readStudent(i);
-                insertEndDynamicArray(ptr, newStud, i);
-                break;
-            default:
-                printf("Invalid choice\n");
-            }
+            enterStudentDynamicArray(i, N, &ptr);
         }
         printListDynamicArray(ptr, i+1);
     }
+
+    char choice;
+    printf("\nDo you want to add more students? Enter Y to add more or N to stop\n");
+    printf("Enter Choice: ");
+    scanf(" %c", &choice);
+    while(choice == 'Y')
+    {
+        int M;
+        printf("Enter Number of students: ");
+        scanf("%d", &M);
+        N = N + M;
+        ptr=(struct Student*)realloc(ptr, N * sizeof(struct Student));
+        if (ptr == NULL)
+        {
+            printf("\nFailed to realloc");
+        }
+        else
+        {
+            for (i=N-M; i<N; i++)
+            {
+                enterStudentDynamicArray(i, N, &ptr);
+                printListDynamicArray(ptr, i+1);
+            }
+
+            printf("\nDo you want to add more students? Enter Y to add more or N to stop\n");
+            printf("Enter Choice: ");
+            scanf(" %c", &choice);
+        }
+    }
+
     free(ptr);
 }
 
@@ -231,7 +327,8 @@ int main()
     printf("Linked List \n");
     startLinkedList();
 
-    printf("\n");
+
+//    printf("\n");
 
     printf("Dynamic Array \n");
     startDynamicArray();
